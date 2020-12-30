226cbcaf6a2c15d489e5119fff6e892d0ef7ca9f
YeonggilGo <dudrlf1859@naver.com>
2020-12-30 17:35:40 +0900

    1230 section 2

---
+++ accountsExam/src/main/java/practice/accountsExam/AccountsExamApplication.java
@@ -0,0 +1,13 @@
+package practice.accountsExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsExamApplication.class, args);
+	}
+
+}
---
+++ accountsExam/src/test/java/practice/accountsExam/AccountsExamApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.accountsExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
